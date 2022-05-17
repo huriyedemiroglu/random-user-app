fe48b1d1245420e05ce49a5afa78e049935b00ca
huriyedemiroglu <demirogluhu@gmail.com>
2022-05-16 18:49:15 -0700

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useEffect } from 'react'
+import {useState} from "react"
+import "./App.css";
+import emailSvg from "./assets/email.svg";
+import phoneSvg from "./assets/phone.svg";
+import locationSvg from "./assets/location.svg";
+import axios from 'axios';
+
 
 function App() {
-  return (
+const [user, setUser] = useState([])
+
+useEffect(() => {
+  fetchUser();
+}, []);
+
+const fetchUser = async()=>{
+   const {data} = await axios("https://randomuser.me/api/");
+ 
+    setUser(data.results);
+    console.log(data);
+  };
+ 
+
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      {user?.map((item,index)=>(
+        <div key ={index} className="card-wrapper">
+          <div className="header-container">
+            <img src={item.picture.large} alt="user"/>
+            <p className="header">
+              {item.name.title} {item.name.first} {item.name.last}
+              </p> 
+        </div>
+        <div className="par-container">
+          <img src={emailSvg} alt="" className="icon" />
+          <p className="par">{item.email}</p>
+       </div>
+
+       <div className="par-container">
+         <img src={phoneSvg} alt="" className="icon" />
+         <p className="par">{item.cell}</p>
+       </div>
+       <div className="par-container">
+         <img src={locationSvg} alt="" className="icon" />
+         <p className="par">
+           {item.location.city} -{item.location.country}
+         </p>
+       </div>
+       <p> Age: {item.dob.age} </p>
+       <p>Register Date: {item.registered.date.substr(0, 10)}</p>
+       </div>
+      ))}
+      <button onClick={fetchUser}>Random User</button>
+      </div>
+ );
+ }
 
 export default App;
+
